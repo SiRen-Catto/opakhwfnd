ed3eb65350383e6f6d3971065721985c95347f99
SiRen-Catto <romanlssiren@gmail.com>
2026-06-03 20:10:13 +0800

    Add files via upload

---
+++ manifest.js
@@ -0,0 +1,16 @@
+{
+  "name": "WindowSR",
+  "short_name": "WindowSR",
+  "start_url": "/index.html",
+  "display": "standalone", 
+  "background_color": "#ffffff",
+  "theme_color": "#ffffff",
+  "icons": [
+
+    {
+      "src": "https://s1.imagehub.cc/images/2026/05/27/4ee125ea6dac219e41e18433b9bbeec4.jpg",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
